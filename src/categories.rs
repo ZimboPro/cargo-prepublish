41df84a2dd8daa45a2dd1b4a398655b8932cdc1f
Leslie <dejagerleslie@gmail.com>
2023-02-11 17:18:32 +0200

    Fix duplicate category entry

--- src/categories.rs
+++ src/categories.rs
@@ -10,19 +10,17 @@ pub fn set_categories(package: &mut Package, args: &Args) {
       );
       let mut category = input::<String>().get();
       let mut cat_inputs = Vec::new();
-      while cat_inputs.is_empty() {
-        if category.contains(" ") {
-          category = category.replace(" ", "-");
-        }
-        if !category.is_empty() {
-          cat_inputs.push(category.clone().to_lowercase());
-        } else {
-          println!("Please enter at least one");
-          category = input::<String>().get();
-        }
+      while category.is_empty() || category.trim().is_empty() {
+        println!("Please enter at least one");
+        category = input::<String>().get();
       }
+
       println!("Please enter more categories. (Press enter to skip)");
       while !category.is_empty() {
+        if category.trim().is_empty() {
+          println!("Please enter a valid word");
+          continue;
+        }
         if category.contains(" ") {
           category = category.replace(" ", "-");
         }

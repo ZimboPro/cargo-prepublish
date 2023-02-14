79cae8593caf0ac3156b96f4190010a1b402c962
Leslie de Jager <leslie@bbd.co.za>
2023-02-14 08:39:28 +0200

    Add comment for categories

--- src/categories.rs
+++ src/categories.rs
@@ -18,13 +18,15 @@ pub fn set_categories_toml(package: &mut Item, args: &Args) {
     && package[CATEGORY_KEY].is_array()
     && !package[CATEGORY_KEY].as_array().unwrap().is_empty();
   if !have_categories && !args.non_interactive {
-    // let cat = "[] # You can find the supported categories here https://crates.io/category_slugs".parse::<toml_edit::Item>().unwrap();
-    package[CATEGORY_KEY] = toml_edit::Item::Value(Value::Array(toml_edit::Array::new()));
+    let cat = format!("{CATEGORY_KEY} = [] # You can find the supported categories here https://crates.io/category_slugs").parse::<toml_edit::Document>().unwrap();
+    let cat = cat.get(CATEGORY_KEY).unwrap();
+    package[CATEGORY_KEY] = cat.clone();
     println!(
       r#"Categories are not set. Please enter at least one category manually. You can find the supported categories here https://crates.io/category_slugs"#
     );
   } else if !have_categories {
-    // let cat = "[] # You can find the supported categories here https://crates.io/category_slugs".parse::<toml_edit::Item>().unwrap();
-    package[CATEGORY_KEY] = toml_edit::Item::Value(Value::Array(toml_edit::Array::new()));
+    let cat = format!("{CATEGORY_KEY} = [] # You can find the supported categories here https://crates.io/category_slugs").parse::<toml_edit::Document>().unwrap();
+    let cat = cat.get(CATEGORY_KEY).unwrap();
+    package[CATEGORY_KEY] = cat.clone();
   }
 }
